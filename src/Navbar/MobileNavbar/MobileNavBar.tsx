0707b896f13d25a5578cb04b14c840c092181b3f
Madan <naikmadan49@gmail.com>
2024-09-22 11:05:49 +0530

    added some addditional check

--- src/Navbar/MobileNavbar/MobileNavBar.tsx
+++ src/Navbar/MobileNavbar/MobileNavBar.tsx
@@ -1,25 +1,25 @@
-import * as React from "react";
-import { useRef } from "react";
-import { motion, sync, useCycle } from "framer-motion";
-import { useDimensions } from "./use-dimensions";
-import "./styles.css";
-import { Navigation } from "./Navigation";
-import { MenuToggle } from "./MobileTogle";
+import * as React from 'react';
+import { useRef } from 'react';
+import { motion, sync, useCycle } from 'framer-motion';
+import { useDimensions } from './use-dimensions';
+import './styles.css';
+import { Navigation } from './Navigation';
+import { MenuToggle } from './MobileTogle';
 
 const sidebar = {
   open: (height = 1000) => ({
     clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
     transition: {
-      type: "spring",
+      type: 'spring',
       stiffness: 20,
       restDelta: 2,
     },
   }),
   closed: {
-    clipPath: "circle(30px at 40px 40px)",
+    clipPath: 'circle(30px at 40px 40px)',
     transition: {
       delay: 0.5,
-      type: "spring",
+      type: 'spring',
       stiffness: 400,
       damping: 40,
     },
@@ -29,13 +29,13 @@ const sidebar = {
 export const MobileNavBar = () => {
   const [isOpen, toggleOpen] = useCycle(false, true);
   const containerRef = useRef(null);
-  const { height } = useDimensions(containerRef);
+  const { height } = useDimensions(containerRef as any);
 
   return (
     <motion.nav
       className="mobile_navigation_bar"
       initial={false}
-      animate={isOpen ? "open" : "closed"}
+      animate={isOpen ? 'open' : 'closed'}
       custom={height}
       ref={containerRef}
     >
